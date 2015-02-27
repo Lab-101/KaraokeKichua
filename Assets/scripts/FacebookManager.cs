074a20d79ff409d9a4b6427df7925e32772ceecf
Diego Castellanos <castsab@gmail.com>
2015-02-27 15:17:46 -0500

    Se agregar prefab de facebook

---
+++ Assets/scripts/FacebookManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FacebookManager : MonoBehaviour {
+
+	public Button loginButton;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
