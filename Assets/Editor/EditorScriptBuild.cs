99df525226dc5a3aff1abf6a03d9e3776e07ba80
Dayana Alejo <dayanalejo@gmail.com>
2015-02-27 16:44:01 -0500

    Automatización De Jenkins

---
+++ Assets/Editor/EditorScriptBuild.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+
+class EditorScriptBuild {
+
+		static string[] SCENES = FindEnabledEditorScenes();
+		
+		static string APP_NAME = "KaraokeKichua-Iphone";
+		static string TARGET_DIR = "/Users/Shared/Jenkins/Home/workspace/KaraokeKichua-Iphone";
+		
+		[MenuItem ("Custom/CI/Build Mac OS X")]
+		static void PerformMacIphone()
+		{
+			string target_dir = APP_NAME + "";
+		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.iPhone,BuildOptions.None);
+		}
+
+		static void PerformAndroid()
+		{
+			string target_dir = APP_NAME + ".apk";
+			GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.Android,BuildOptions.None);
+		}	
+
+		private static string[] FindEnabledEditorScenes() {
+			List<string> EditorScenes = new List<string>();
+			foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+				if (!scene.enabled) continue;
+				EditorScenes.Add(scene.path);
+			}
+			return EditorScenes.ToArray();
+		}
+		
+		static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+		{
+			EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+			string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
+			if (res.Length > 0) {
+				throw new Exception("BuildPlayer failure: " + res);
+			}
+		}
+	}
+
