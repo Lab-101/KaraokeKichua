edadb9f52236addb027493ab920176fc6be08aa9
Diego Castellanos <castsab@gmail.com>
2015-02-27 17:08:23 -0500

    Se asocia texto de cargando al momento de oprimir login

--- Assets/scripts/FacebookManager.cs
+++ Assets/scripts/FacebookManager.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 public class FacebookManager : MonoBehaviour {
 
 	public Button loginButton;
+	public Text loadingMessage;
+	private string permissions = "public_profile,email,user_friends";
 
-	// Use this for initialization
 	void Start () {
 
 		loginButton.onClick.AddListener (delegate {
@@ -18,8 +19,25 @@ public class FacebookManager : MonoBehaviour {
 	
 	}
 	
-	private void HandleLoginSelected ()
-	{
-		Debug.Log ("Se presiono el boton de login");
+	private void HandleLoginSelected () {
+		loadingMessage.gameObject.SetActive(true);
+		loginButton.gameObject.SetActive (false);
+		FB.Init(HandleInitComplete);
+	}
+
+	private void HandleInitComplete () {
+		if (!FB.IsLoggedIn)
+			FB.Login(permissions, HandleLoginComplete);
+		HideLoginButton ();
+	}
+
+	private void HandleLoginComplete(FBResult result) {
+		Debug.Log ("El usuario esta logiao: "+result.ToString());
+		HideLoginButton ();
+	}
+
+	private void HideLoginButton(){
+		loadingMessage.gameObject.SetActive (false);
+		loginButton.gameObject.SetActive (false);
 	}
 }
