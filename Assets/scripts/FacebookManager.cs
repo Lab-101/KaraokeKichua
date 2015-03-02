892d16dac6a03c7b96fb8cb722a58aa85e2b4b46
Diego Castellanos <castsab@gmail.com>
2015-03-02 11:36:10 -0500

    Se ignoro el folder de builds y se ajusto funcioalidad de login

--- Assets/scripts/FacebookManager.cs
+++ Assets/scripts/FacebookManager.cs
@@ -16,12 +16,11 @@ public class FacebookManager : MonoBehaviour {
 	}
 	
 	void Update () {
-	
+
 	}
 	
 	private void HandleLoginSelected () {
-		loadingMessage.gameObject.SetActive(true);
-		loginButton.gameObject.SetActive (false);
+		ShowLoading ();
 		FB.Init(HandleInitComplete);
 	}
 
@@ -32,12 +31,33 @@ public class FacebookManager : MonoBehaviour {
 	}
 
 	private void HandleLoginComplete(FBResult result) {
-		Debug.Log ("El usuario esta logiao: "+result.ToString());
-		HideLoginButton ();
+
+		if (FB.IsLoggedIn) {
+		
+				Debug.Log ("El usuario esta logiao: " + result.ToString ());
+				HideLoginButton ();
+		} else {
+
+			ShowLoginButton();
+		}
+
+
 	}
 
 	private void HideLoginButton(){
 		loadingMessage.gameObject.SetActive (false);
+		loginButton.gameObject.SetActive (false);
+	}
+
+	private void ShowLoginButton ()
+	{
+		loadingMessage.gameObject.SetActive(false);
+		loginButton.gameObject.SetActive (true);
+
+	}
+
+	private void ShowLoading(){
+		loadingMessage.gameObject.SetActive(true);
 		loginButton.gameObject.SetActive (false);
 	}
 }
