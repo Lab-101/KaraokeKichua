03de083e3fd77f35e0f95e8ad35a7d0ee1a2403e
Diego Castellanos <castsab@gmail.com>
2015-03-02 12:10:31 -0500

    se pinta el nombre del usuario que inicio sesion

--- Assets/scripts/FacebookManager.cs
+++ Assets/scripts/FacebookManager.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
 public class FacebookManager : MonoBehaviour {
 
 	public Button loginButton;
 	public Text loadingMessage;
+	public Text sessionMessage;
 	private string permissions = "public_profile,email,user_friends";
 
 	void Start () {
@@ -31,17 +33,11 @@ public class FacebookManager : MonoBehaviour {
 	}
 
 	private void HandleLoginComplete(FBResult result) {
-
 		if (FB.IsLoggedIn) {
-		
-				Debug.Log ("El usuario esta logiao: " + result.ToString ());
-				HideLoginButton ();
+			ObtainUserInfo();
 		} else {
-
 			ShowLoginButton();
 		}
-
-
 	}
 
 	private void HideLoginButton(){
@@ -53,11 +49,33 @@ public class FacebookManager : MonoBehaviour {
 	{
 		loadingMessage.gameObject.SetActive(false);
 		loginButton.gameObject.SetActive (true);
-
 	}
 
 	private void ShowLoading(){
 		loadingMessage.gameObject.SetActive(true);
 		loginButton.gameObject.SetActive (false);
+	}
+
+	private void ObtainUserInfo(){
+		FB.API("/me?fields=first_name", Facebook.HttpMethod.GET, HandleUserInfoObtained);
+	}
+
+	private void HandleUserInfoObtained(FBResult result)
+	{
+		if (result.Error != null)
+			Debug.Log("Error Response:\n" + result.Error);
+		else if (!FB.IsLoggedIn)
+			Debug.Log("Login cancelled by Player");
+		else{
+			IDictionary dict = Facebook.MiniJSON.Json.Deserialize(result.Text) as IDictionary;
+			ShowLoggedInMessage(dict["first_name"].ToString());
+		}
+	}
+
+	private void ShowLoggedInMessage(string message)
+	{
+		HideLoginButton ();
+		sessionMessage.gameObject.SetActive (true);
+		sessionMessage.text = sessionMessage.text + " " + message;
 	}
 }
