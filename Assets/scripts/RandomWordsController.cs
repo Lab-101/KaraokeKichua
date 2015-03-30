16cba34a24d2233a9dd1dca0bfd833fc0571ed81
yesiddaza <yiye_91@hotmail.com>
2015-03-30 17:07:27 -0500

    Se crean y se muestran aleatoriamente palabras

---
+++ Assets/scripts/RandomWordsController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
+
+public class RandomWordsController : MonoBehaviour {
+
+	public Button wordPrefab;
+	private List<Button> randomWordsButtons = new List<Button>();
+	private List<string> randomWords = new List<string>();
+
+	void Start(){
+		randomWords.Add ("uno");
+		randomWords.Add ("dos");
+		randomWords.Add ("tres");
+		randomWords.Add ("cuatro");
+		randomWords.Add ("cinco");
+		randomWords.Shuffle ();
+		DrawButtonsByWord(randomWords);
+	}
+
+	public void DrawButtonsByWord(List<string> wordList){
+		foreach(string word in wordList){
+			randomWordsButtons.Add(createWordButton(word));
+		}
+	}
+
+	private Button createWordButton(string name){
+		Button newItem;
+		newItem = Instantiate(wordPrefab) as Button;
+		newItem.name = name;
+		newItem.transform.SetParent(gameObject.transform, false);	
+		newItem.transform.GetChild(0).GetComponent<Text>().text = name;
+		return newItem;
+	}
+}
