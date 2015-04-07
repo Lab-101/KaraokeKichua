b8d301ef99cb00e94f22151087f2d50c5551eeca
Diego Castellanos <castsab@gmail.com>
2015-04-07 15:17:04 -0500

    Se actualiza estructura wordActivity.json y se actualiza el prefab del karaoque

---
+++ Assets/scripts/Map.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+	[SerializeField]
+	private Button levelButton;
+	[SerializeField]
+	private Button levelTwoButton;
+
+
+
+}
