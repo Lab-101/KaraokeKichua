8225e2ad3b8cbe8f4fd4e3a86508a62ffea37750
Diego Castellanos <castsab@gmail.com>
2015-04-07 16:00:02 -0500

    Se crear script para el mapa

--- Assets/scripts/Map.cs
+++ Assets/scripts/Map.cs
@@ -5,10 +5,31 @@ using System.Collections;
 public class Map : MonoBehaviour {
 
 	[SerializeField]
-	private Button levelButton;
+	private Button levelOneButton;
 	[SerializeField]
 	private Button levelTwoButton;
 
+	void Start()
+	{
+		IdentifyActiveLevel (levelOneButton);
 
+		levelOneButton.onClick.AddListener(delegate {
+			IdentifyCurrentLevel (levelOneButton);
+		} );
+
+		levelTwoButton.onClick.AddListener(delegate {
+			IdentifyCurrentLevel (levelTwoButton);
+		} );
+	}
+
+	private void IdentifyCurrentLevel (Button levelButton)	{
+		levelButton.image.color = Color.green;
+	}
+
+	private void IdentifyActiveLevel (Button levelButton)
+	{
+		levelButton.enabled = true;
+		levelButton.image.color = Color.red;
+	}
 
 }
