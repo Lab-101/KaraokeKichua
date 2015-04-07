5d7ba5c5b59cca54caac68d66fae4c1a11047014
emiliop <milopenaloza@gmail.com>
2015-04-07 16:32:27 -0500

    Se crea ventana de intro

---
+++ Assets/scripts/IntroController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class IntroController : MonoBehaviour {
+
+	[SerializeField]
+	public Button introButton;
+	[SerializeField]
+	public Button returnButton;
+	[SerializeField]
+	private GameObject introScreen;
+	// Use this for initialization
+	void Start () {
+		introButton.onClick.AddListener(delegate {
+			introScreen.SetActive(true);
+		} );
+		returnButton.onClick.AddListener(delegate {
+			introScreen.SetActive(false);
+		} );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
