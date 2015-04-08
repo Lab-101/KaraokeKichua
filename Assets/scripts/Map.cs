558c4380469f8068041224f82c643ff3e15b5ac2
Diego Castellanos <castsab@gmail.com>
2015-04-08 12:15:50 -0500

    Se crean los 10 niveles sobre el mapa, con sus actividades

--- Assets/scripts/Map.cs
+++ Assets/scripts/Map.cs
@@ -8,12 +8,30 @@ public class Map : MonoBehaviour {
 	private Button levelOneButton;
 	[SerializeField]
 	private Button levelTwoButton;
+	[SerializeField]
+	private Button levelThreeButton;
+	[SerializeField]
+	private Button levelFourButton;
+	[SerializeField]
+	private Button levelFiveButton;
+	[SerializeField]
+	private Button levelSixButton;
+	[SerializeField]
+	private Button levelSevenButton;
+	[SerializeField]
+	private Button levelEigthButton;
+	[SerializeField]
+	private Button levelNineButton;
+	[SerializeField]
+	private Button levelTenButton;
 
 	private int numberCurrentLevel;
 
 	void Start()
 	{
-		
+
+		IdentifyInactiveLevels ();
+
 		levelOneButton.onClick.AddListener(delegate {
 			IdentifyInactiveLevels ();
 			IdentifyCurrentLevel(levelOneButton);
@@ -41,6 +59,14 @@ public class Map : MonoBehaviour {
 	{
 		levelOneButton.image.color = Color.grey;
 		levelTwoButton.image.color = Color.grey;
+		levelThreeButton.image.color = Color.grey;
+		levelFourButton.image.color = Color.grey;
+		levelFiveButton.image.color = Color.grey;
+		levelSixButton.image.color = Color.grey;
+		levelSevenButton.image.color = Color.grey;
+		levelEigthButton.image.color = Color.grey;
+		levelNineButton.image.color = Color.grey;
+		levelTenButton.image.color = Color.grey;
 	}
 
 }
