709bc5d9145a9a17f44125b71af8662a7deec00a
emiliop <milopenaloza@gmail.com>
2015-04-10 11:27:13 -0500

    Ahora si el intro lleva al WriteActivity solo la primera vez

--- Assets/scripts/IntroController.cs
+++ Assets/scripts/IntroController.cs
@@ -11,15 +11,45 @@ public class IntroController : MonoBehaviour {
 	public Button returnButton;
 	[SerializeField]
 	private GameObject introScreen;
-	// Use this for initialization
-	void Start () {
-		introButton.onClick.AddListener(delegate {
-			introScreen.SetActive(true);
-		} );
-		returnButton.onClick.AddListener(delegate {
-			introScreen.SetActive(false);
-		} );
+	[SerializeField]
+	private bool introScreenItsOpened;
+	[SerializeField]
+	private static readonly object syncLock = new object();
+	[SerializeField]
+	private Activity wordActivity;
 
+	// Use this for initialization
+	void Awake () {
+		introScreenItsOpened = false;
+		ActiveIntroScreen ();
+		returnButton.onClick.AddListener (delegate {
+			OpenIntroScreenFirstTime();
+			DeactiveIntroScreen ();
+		});
+	}
+
+	private void OpenIntroScreenFirstTime(){
+		lock (syncLock) {
+			if (!introScreenItsOpened) { 
+					wordActivity.StartActivity ();
+					introScreen.SetActive (false);
+					introScreenItsOpened = true;
+			}
+		}
+	}
+
+	void DeactiveIntroScreen ()
+	{
+		returnButton.onClick.AddListener (delegate {
+			introScreen.SetActive (false);
+		});
+	}
+
+	void ActiveIntroScreen ()
+	{
+		introButton.onClick.AddListener (delegate {
+			introScreen.SetActive (true);
+		});
 	}
 	
 	// Update is called once per frame
