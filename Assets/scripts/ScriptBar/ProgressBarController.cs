b28584b890f58bb02980596b72f5e2dc266f1c6e
yesiddaza <yiye_91@hotmail.com>
2015-04-16 17:21:26 -0500

    Se arregla barra en iPad

--- Assets/scripts/ScriptBar/ProgressBarController.cs
+++ Assets/scripts/ScriptBar/ProgressBarController.cs
@@ -10,11 +10,11 @@ public class  ProgressBarController : MonoBehaviour
 
 	public void SetFillerSize(int levelActivity)
 	{
-		float defaultWidth = 1280;
-		barCharge.sizeDelta = new Vector2 ((defaultWidth * 0.31f) , (defaultWidth * 0.057f));
+		float defaultWidth = 1280f * ((float)Screen.width / (float)Screen.height) / (16f / 9f);
+		barCharge.sizeDelta = new Vector2 ((defaultWidth * 0.31f) , 73f);
 		barCharge.anchoredPosition = new Vector2 ( 0.0f , 0.0f );
 		fillerCharge.sizeDelta = new Vector2 ( barCharge.sizeDelta.x, barCharge.sizeDelta.y );
 		float maxBarWidth = (barCharge.sizeDelta.x * levelActivity / 12f);
-		fillerCharge.sizeDelta = new Vector2 (maxBarWidth , (defaultWidth * 0.057f));
+		fillerCharge.sizeDelta = new Vector2 (maxBarWidth , fillerCharge.sizeDelta.y);
 	}
 }
