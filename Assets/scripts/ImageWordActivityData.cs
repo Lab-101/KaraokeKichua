03d2a9a88d9726d09f3d8223398341940993bd4a
Diego Castellanos <castsab@gmail.com>
2015-04-20 12:03:05 -0500

    Se crean clases para el parser de la estructura de datos para la actividad ImageWordActivity

---
+++ Assets/scripts/ImageWordActivityData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[Serializable]
+public class ImageWordActivityData {
+
+	public string songName;
+	public List<string> wordsList = new List<string> ();
+	public string wordValid;
+	public string wordTranslated;
+
+}
---
+++ Assets/scripts/JsonImageWordParser.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+
+public class JsonImageWordParser : MonoBehaviour {
+
+	private int levelFilter; 
+	private ImageWordActivityData data;
+	
+	public void SetLevelFilter(int level) {
+		this.levelFilter = level;
+	}
+	
+	public ImageWordActivityData Data {
+		get {
+			return data;
+		}
+	}
+	
+	public string JSONString {
+		set {
+			data = CreateWordsDataList (Json.Deserialize (value)as List<object>);
+		}
+	}
+	
+	private ImageWordActivityData CreateWordsDataList(List<object> levels) {
+		ImageWordActivityData data = new ImageWordActivityData ();
+		foreach(Dictionary<string, object> level in levels){
+			if( levelFilter.ToString() == level["level"].ToString() )
+				data = CreateWordActivityDataObject(level);
+		}
+		return data;
+	}
+	
+	private ImageWordActivityData CreateWordActivityDataObject (Dictionary<string, object> levelData)	{
+		ImageWordActivityData data = new ImageWordActivityData ();
+		data.songName = levelData ["name"].ToString();
+		data.wordsList = CreateWordsList( levelData ["showWords"] as List<object>);
+		data.wordValid = levelData["wordValid"].ToString();
+		data.wordTranslated = levelData["wordTranslated"].ToString();
+		return data;
+	}
+	
+	private List<string> CreateWordsList (List<object> words)	{
+		List<string> wordsList = new List<string> ();
+		foreach(string word in words){
+			wordsList.Add(word.ToString());
+		}
+		return wordsList;
+		
+	}
+}
