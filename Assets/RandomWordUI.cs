eaad5e05d5ed616ae8e80305868a6b7f15ead010
yesiddaza <yiye_91@hotmail.com>
2015-04-21 12:56:05 -0500

    Arreglos de diseño y palabras aleatorias en PhraseActivity

---
+++ Assets/RandomWordUI.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
+
+public class RandomWordUI : MonoBehaviour {
+
+	public Button wordPrefab;
+	private List<Button> randomWordsButtons = new List<Button>();
+	public Action <Button> RandomWordSelected;
+	
+	public Action SelectedCorrectWords {
+		get;
+		set;
+	}
+	
+	public void DrawButtonsByWord(List<Word> wordList){
+		wordList.Shuffle ();
+		foreach(Word word in wordList){
+			randomWordsButtons.Add(createWordButton(word.text));
+		}
+	}
+
+	private Button createWordButton(string name){
+		Button newItem;
+		newItem = Instantiate(wordPrefab) as Button;
+		newItem.name = name;
+		newItem.transform.SetParent(gameObject.transform, false);	
+		newItem.transform.GetChild(0).GetComponent<Text>().text = name;
+		newItem.onClick.AddListener (delegate {
+			if(RandomWordSelected!=null)
+				RandomWordSelected(newItem);
+		});
+		return newItem;
+	}
+	
+	public void DisableAllButtons(){
+		foreach(Button word in randomWordsButtons){
+			if(word != null )word.interactable = false;
+		}
+		FinishGame ();
+	}
+	
+	private void FinishGame(){
+		if (SelectedCorrectWords!=null)
+			SelectedCorrectWords();
+	}
+}
--- Assets/scripts/PhraseActivity.cs
+++ Assets/scripts/PhraseActivity.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PhraseActivity : Activity {
 
 	public TextAsset json;
 	private PhraseActivityData data = new PhraseActivityData ();
 	private Phrase phrase = new Phrase();
+	[SerializeField]
+	private Text phraseTranslatedField;
+	[SerializeField]
+	private RandomWordUI randomWordsPhrase;
 
 	void Awake(){
 		if (level != null) {
@@ -34,10 +41,27 @@ public class PhraseActivity : Activity {
 	}
 
 	private void HandleActivityStarted () {
+		//ClearActivity ();
+		CreateActivity ();
 		BeginActivity ();
+	}
+
+	private void CreateActivity () {
+		phrase = GetRandomPhrase (data.phrases);
+		phraseTranslatedField.text = phrase.phraseTranslated;
+		randomWordsPhrase.DrawButtonsByWord (phrase.words);
+		result.RetryActionExecuted = StartActivity;
 	}
 
 	private void BeginActivity ()	{
 		gameStateBehaviour.GameState = GameState.PharseActivity;
+	}
+
+	private Phrase GetRandomPhrase(List<Phrase> phrases){
+		if (phrases.Count != 0) {
+			int randomIndex = UnityEngine.Random.Range(0, phrases.Count);
+			return phrases[randomIndex];		
+		}
+		throw new Exception ("El nivel" + GameSettings.Instance.nameLevel [0] + "no tiene frases");
 	}
 }
