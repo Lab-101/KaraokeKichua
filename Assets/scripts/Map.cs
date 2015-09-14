2ffbaa295d491a7708576786bc7041dd6b63c6d9
cbellom <cbellom@unal.edu.co>
2015-09-14 10:07:45 -0500

    Se cambia nombre de la aplicacion. Se cambia URL del diccionario. Se cambia mensaje de inicio

--- Assets/scripts/Map.cs
+++ Assets/scripts/Map.cs
@@ -109,7 +109,7 @@ public class Map : MonoBehaviour {
 			level.UnlockNextLevel = HandleUnlockNextLevel;
 			if (isLevelUnlocked) {
 				indexLevelSelected = index;
-				welcomePanel.transform.FindChild("WelcomeMessage").GetComponent<Text>().text = (index != 0)? "Contin\u00FAa tu viaje en el <b>Nivel " + levelList[index].numberLevel +"</b>" : "\u00A1Inicia el viaje en <b>Otavalo</b>!";
+				welcomePanel.transform.FindChild("WelcomeMessage").GetComponent<Text>().text = (index != 0)? "Contin\u00FAa tu viaje en el <b>Nivel " + levelList[index].numberLevel +"</b>" : "\u00A1Inicia el viaje en <b>Otavalo</b>! haciendo click en el nivel 1";
 			}
 		}
 	}
